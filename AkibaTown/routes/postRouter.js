8bfc3422b4cb70458a045562124451c2b2981a54
thibault-kine <thibault.kine@laplateforme.io>
2023-01-05 15:56:16 +0100

    Update routes

--- AkibaTown/routes/postRouter.js
+++ AkibaTown/routes/postRouter.js
@@ -14,34 +14,34 @@ const {
 
 
 
-app.post('/', (req, res) => {
+app.post('/posts/', (req, res) => {
     createPost(req, res);
 })
 
 
-app.get('/all', (req, res) => {
+app.get('/posts/all', (req, res) => {
     getAllPosts(req, res);
 })
 
-app.get('/{id}', (req, res) => {
+app.get('/posts/{id}', (req, res) => {
     getPostByID(req, res);
 })
 
-app.get('/all?userID={userID}', (req, res) => {
+app.get('/posts/all?userID={userID}', (req, res) => {
     getAllPostsByUserID(req, res);
 })
 
-app.get('/all?title={title}', (req, res) => {
+app.get('/posts/all?title={title}', (req, res) => {
     getPostByTitle(req, res);
 })
 
 
-app.put('/{id}', (req, res) => {
+app.put('/posts/{id}', (req, res) => {
     updatePost(req, res);
 })
 
 
-app.delete('/{id}', (req, res) => {
+app.delete('/posts/{id}', (req, res) => {
     deletePost(req, res);
 })
 
