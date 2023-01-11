6b8a406d650899a0dae06e4f9c6c72af26b4b13b
ibrahim-sylla <91750360+IBRAHIM-SYLLA@users.noreply.github.com>
2023-01-11 13:51:55 +0100

    check si l'utilisateur et connécté ok

--- API/router/userRouter.js
+++ API/router/userRouter.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { getAll, getOne, createOne, updateOne, deleteOne, login } = require( '../services/userApi');
+const authenticateJWT = require('../utils/authenticateJWT');
 
 router.post("/users/create", (req, res) => {
     createOne(req, res);
 });
 
-router.get("/users/all", (req, res) => {
-    getAll(req, res)
+router.get("/users/all", authenticateJWT, (req, res) => {
+    getAll(req, res);
 });
 
 router.get("/users/all/?id={id}", (req, res) => {
@@ -22,7 +23,7 @@ router.delete("/users/delete/?id={id}", (req, res) => {
     deleteOne(req, res);
 });
 
-router.post("/login", (req, res) => {
+router.post("/users/login", (req, res) => {
     login(req, res);
 });
 
