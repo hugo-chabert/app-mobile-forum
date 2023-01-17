03452cf0d4e385c11217dfc57b60f029dc5df171
thibault-kine <thibault.kine@laplateforme.io>
2023-01-17 14:27:18 +0100

    Background image

---
+++ AkibaTown/constants/BackgroundStyle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet } from 'react-native'
+
+const backgroundStyle = StyleSheet.create({
+    bg: {
+        width: '100%',
+        height: '100%',
+        opacity: 0.4
+    }
+})
+
+export default backgroundStyle
--- AkibaTown/screens/LandingPageScreen.tsx
+++ AkibaTown/screens/LandingPageScreen.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, Image } from 'react-native';
+import { View, Text, Button, StyleSheet, Image, ImageBackground } from 'react-native';
+
+import backgroundStyle from '../constants/BackgroundStyle';
 
 const LandingPage = ({ navigation }) => {
     return (
         <View style={styles.container}>
-            <Image 
-                source={require('../assets/images/AkibaTownLogo.png')}
-                style={styles.bigLogo}
-            />
+            <ImageBackground
+                source={require('../assets/images/background.png')}
+                style={backgroundStyle.bg}
+            ></ImageBackground>
+            <View style={styles.landingPage}>
+                <Image 
+                    source={require('../assets/images/AkibaTownLogo.png')}
+                    style={styles.bigLogo}
+                    />
+            </View>
         </View>
     );
 }
@@ -16,13 +24,25 @@ export default LandingPage;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#DBD9DB'
+        backgroundColor: 'black',
     },
     bigLogo: {
         width: 250,
         height: 250,
+        // shadow
+        shadowColor: '#ffffff',
+        shadowOffset: { width: 0, height: 0 },
+        shadowOpacity: 1,
+        shadowRadius: 10,
+        elevation: 10,
+    },
+    landingPage: {
+        width: "100%",
+        height: "100%",
+        flex: 1,
+        alignItems: 'center',
+        position: 'absolute',
+        top: 0,
+        left: 0
     }
 });
