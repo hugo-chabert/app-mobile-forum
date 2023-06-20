7fd659e0e791e4a215fd04cbe14c7db69e2a9b63
Hugo <hugo.chabert@laplateforme.io>
2023-06-20 13:22:00 +0200

    initial commit

--- AkibaTown/App.js
+++ AkibaTown/App.js
@@ -1,35 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
-import { UserProvider } from './context/UserContext';
-import { PostProvider } from './context/PostContext';
-import { CommentProvider } from './context/CommentContext';
-import { NavigationContainer } from '@react-navigation/native';
-import ConnectionScreen from './containers/connection';
-import SignUpScreen from './containers/SignUpScreen';
-import NewPostscreen from './containers/Newpost';
-
-const Stack = createNativeStackNavigator();
-
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-    const isLoadingComplete = useCachedResources();
-    const colorScheme = useColorScheme();
-
-    if (!isLoadingComplete) {
-        return null;
-    } else {
-        return (
-            <NavigationContainer>
-                <Stack.Navigator>
-                    <Stack.Screen name="SignUp" component={SignUpScreen} />
-                    <Stack.Screen name="SignIn" component={ConnectionScreen} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        );
-    }
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- AkibaTown/config/config.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = "http://10.10.19.225:3000"
-export const API_URL = "http://localhost:3000/api"
