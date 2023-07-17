36884ef09890c0f72274368a4ad84a8673af5a7d
thibault-kine <thibault.kine@laplateforme.io>
2023-07-17 10:26:08 +0200

    Update PostPreviewCard.tsx

--- AkibaTown/components/PostPreviewCard.tsx
+++ AkibaTown/components/PostPreviewCard.tsx
@@ -10,7 +10,7 @@ const PostPreviewCard = ({ type, dataToShow, navigation }: Props) => {
         return (
             <View style={styles?.container}>
                 <View>
-                    <Text style={{ color: 'black', fontSize: 18 }}>0
+                    <Text style={{ color: 'black', fontSize: 18 }}>
                         {dataToShow?.title}
                         <Text style={{ color: 'grey', fontSize: 14 }}>
                             {' '}
