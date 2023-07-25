91a4fb12ac492d5214a57bb19e53132e59bbf4e4
thibault-kine <thibault.kine@laplateforme.io>
2023-07-25 14:09:28 +0200

    Fix merge

--- API/App.js
+++ API/App.js
@@ -10,7 +10,7 @@ const sequelize = require('./src/config/database');
 const bcrypt = require('bcrypt')
 const { users } = require('./UserMock.json'); // Importe les fixtures
 const { posts } = require('./PostMock.json')
-const  User  = require('./src/models/UserContext.js'); // Importe ton modèle User
+const User = require('./src/models/UserContext.js'); // Importe ton modèle User
 const Posts = require('./src/models/PostContext')
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
@@ -21,33 +21,24 @@ app.use(usersRouter);
 app.use(postsRouter);
 app.use(commentsRouter);
 
-<<<<<<< Updated upstream
 const UserModel = sequelize.models.users
 const PostModel = sequelize.models.posts
 sequelize.sync() // ajouté { force: true } pour pouvoir creer de nouveau mock
-.then(() => {
-  // users.map((user) => {
-  //   const hashedPassword = bcrypt.hashSync(user.password, 10); // Le deuxième argument est le "salt rounds"
-  //   user.password = hashedPassword;
-  //   UserModel.create(user);
-  // })
-  // console.log('Connexion database nickel. user created')
+    .then(() => {
+        users.map((user) => {
+            const hashedPassword = bcrypt.hashSync(user.password, 10); // Le deuxième argument est le "salt rounds"
+            user.password = hashedPassword;
+            UserModel.create(user);
+        })
+        console.log('Connexion database nickel. user created')
 
-  posts.map((post) => {
-      PostModel.create(post);
+        posts.map((post) => {
+            PostModel.create(post);
+        })
+        console.log('Connexion database nickel. posts created')
     })
-    console.log('Connexion database nickel. posts created')
+    .catch('errorr')
+const server = app.listen(port, () => {
+    // console.log(`Example app listening on port ${port}`)
 })
-.catch('errorr')
- const server = app.listen(port, () => {
-  // console.log(`Example app listening on port ${port}`)
-    })
 module.exports = server
-=======
-sequelize.sync()
-    .then((console.log('Connexion database done')))
-    .catch(error => console.log(error))
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
->>>>>>> Stashed changes
